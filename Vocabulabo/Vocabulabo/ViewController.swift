de6e1214437df1ca15906907c131efc3360e0217
Shu Nakamura <jumpinthedarkness.shu.adress+git@gmail.com>
2020-07-07 15:36:48 +0900

    ボタンの動作処理を追加。 スムーズに動き押下不可のボタンと、スムーズに動かないが押下可能のボタン2パターンの動作あり。

--- Vocabulabo/Vocabulabo/ViewController.swift
+++ Vocabulabo/Vocabulabo/ViewController.swift
@@ -14,7 +14,7 @@ let boundWidth = UIScreen.main.bounds.size.width
 
 class ViewController: UIViewController {
     var buttonTitles:[WikipediaTitleData]?
-
+    
     let clearSpace = boundWidth / 2
     let wordCount = floor((boundHeight - 50) / 25)
     
@@ -34,36 +34,47 @@ class ViewController: UIViewController {
     
     func buttonSetup(){
         var useButtonList:[UIButton] = []
-        
+        // ボタンの生成
         for i in 0 ..< Int(wordCount){
-                let button: FlowButton = {
-                    let button = FlowButton(frame:CGRect(x: Int(boundWidth + clearSpace), y: 25 + (i * 25), width: 0, height: 0),title:"aaa")
-                    return button
-                }()
+            let button: FlowButton = {
+                let button = FlowButton(frame:CGRect(x: Int(boundWidth + clearSpace), y: 25 + (i * 25), width: 0, height: 0),title:"")
+                button.addTarget(self, action: #selector(buttonAction(word:)), for: .touchDown)
+                return button
+            }()
             useButtonList.append(button)
         }
         
+        // ボタン文字の代入
         for i in 0 ..< Int(wordCount){
             self.view.addSubview(useButtonList[i])
-            let randomDouble = Double.random(in: 7 ... 10)
-
-            var timer1 = Timer.scheduledTimer(withTimeInterval: randomDouble, repeats: true, block: { (timer) in
+            let randomDouble = Double.random(in: 12 ... 16)
+            
+            var timer = Timer.scheduledTimer(withTimeInterval: randomDouble, repeats: true, block: { (timer) in
                 textChange(wordButton: useButtonList[i])
-                textMove(wordButton: useButtonList[i], duration: randomDouble, moveToX: boundWidth + self.clearSpace + self.clearSpace)
-            })
-        }
-
-        func textMove(wordButton:UIButton,duration:TimeInterval,moveToX:CGFloat){
-            UIView.animate(withDuration: duration, delay: 0.0, animations: {
-                wordButton.center.x -= moveToX
-            }, completion: { _ in
-                wordButton.center.x += moveToX
+                //                textMove(wordButton: useButtonList[i], duration: randomDouble, moveToX: boundWidth + self.clearSpace + self.clearSpace)
+                textMove(wordButton: useButtonList[i], time: randomDouble, moveToX: boundWidth + self.clearSpace + self.clearSpace)
             })
         }
         
         func textChange(wordButton:UIButton){
             wordButton.setTitle(self.buttonTitles!.randomElement()!.titleName, for: .normal)
             wordButton.sizeToFit()
+        }
+        
+        /// 未完成、タップ不可能だがイメージ通りの動作パターン
+        func textMove(wordButton:UIButton,duration:TimeInterval,moveToX:CGFloat){
+            UIView.animate(withDuration: duration, delay: 0, animations: {
+                wordButton.center.x -= moveToX
+            }, completion: { _ in
+                wordButton.center.x += moveToX
+            })
+        }
+        
+        ///  未完成、タップ可能なボタンの動作パターン
+        func textMove(wordButton:UIButton,time:Double,moveToX:CGFloat){
+            var timer = Timer.scheduledTimer(withTimeInterval: time / 100, repeats: true, block: { (timer) in
+                wordButton.center.x -= moveToX / 100
+            })
         }
     }
     
@@ -95,4 +106,17 @@ class ViewController: UIViewController {
             }
         }
     }
+    
+    @objc func buttonAction(word:UIButton){
+        let wordStr = word.titleLabel?.text ?? ""
+        let urlString = "https://ja.wikipedia.org/wiki/" + wordStr
+        guard let encodeUrlString: String = urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
+            let url = URL(string:encodeUrlString) else{
+                return
+        }
+        if (UIApplication.shared.canOpenURL(url)) {
+            UIApplication.shared.open(url)
+        }
+    }
 }
+
